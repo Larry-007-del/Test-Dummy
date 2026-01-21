a03757d369ecb6b8abcdcaf10d622ab533b7f969
Larry-007-del <175581182+Larry-007-del@users.noreply.github.com>
2026-01-21 11:13:41 +0000

    Enable SessionAuthentication for DRF browsable API login/logout

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -148,6 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Django REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
